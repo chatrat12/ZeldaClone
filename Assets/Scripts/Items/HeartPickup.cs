ac4eb7d3f368b17c4c6e86b89a60b51328815c13
Matthew Thompson <chatrat12@gmail.com>
2017-11-03 18:24:55 -0400

    Added HeartPickup

---
+++ Assets/Scripts/Items/HeartPickup.cs
@@ -0,0 +1,12 @@
+﻿class HeartPickup : ItemPickup
+{
+    public override void AddToCollection(ItemCollection collection)
+    {
+        var character = collection.GetComponent<Character>();
+        if(character != null)
+        {
+            base.AddToCollection(collection);
+            Damage.ApplyGenericDamage(character.gameObject, -4);
+        }
+    }
+}
