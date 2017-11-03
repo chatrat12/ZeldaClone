c06e8a76e4ecead844c11139536b7602791aef35
Matthew Thompson <chatrat12@gmail.com>
2017-11-02 23:39:12 -0400

    Started key HUD

---
+++ Assets/Scripts/UI/HUD/KeysHUD.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class KeysHUD : MonoBehaviour
+{
+    private Text _text;
+    private ItemCollection _playerItemCollection;
+
+    private int _previousNumberOfKeys = 0;
+
+    private void Awake()
+    {
+        _text = GetComponentInChildren<Text>();
+    }
+    private void Start()
+    {
+        _playerItemCollection = GameObject.FindObjectOfType<PlayerController>()
+            .GetComponent<ItemCollection>();
+        SetKeyNumberText(_playerItemCollection.SilverKeyCount);
+    }
+
+    private void LateUpdate()
+    {
+        if (_playerItemCollection.SilverKeyCount != _previousNumberOfKeys)
+            SetKeyNumberText(_playerItemCollection.SilverKeyCount);
+    }
+
+    private void SetKeyNumberText(int numberOfKeys)
+    {
+        _text.text = numberOfKeys.ToString();
+        _previousNumberOfKeys = numberOfKeys;
+    }
+
+}
