fbbed2e7ee38c6ce9784f86e58b4698c946c96cd
Matthew Thompson <chatrat12@gmail.com>
2017-11-10 13:05:04 -0500

    Started new dungeon for one week build.

---
+++ Assets/Scripts/Props/Mechanisms/ActivateAnimationWithMechanism.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ActivateAnimationWithMechanism : MonoBehaviour
+{
+    [SerializeField]
+    private Mechanism _mechanism;
+
+    private Animator _animator;
+    
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _mechanism.Activated += delegate
+        {
+            _animator.SetTrigger("Activate");
+        };
+    }
+}
