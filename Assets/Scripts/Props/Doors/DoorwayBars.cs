6870caeff1ad0ecb09e7fd9f54adda421a59f6d8
Matthew Thompson <chatrat12@gmail.com>
2017-11-09 21:23:46 -0500

    Added DoorwayBars and OrbSwitch

---
+++ Assets/Scripts/Props/Doors/DoorwayBars.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class DoorwayBars : MonoBehaviour
+{
+    [SerializeField]
+    private bool _startOpen = true;
+    [SerializeField]
+    private Mechanism _mechanism;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        if (_startOpen)
+            _animator.SetTrigger("OpenOnStart");
+
+        if(_mechanism != null)
+        {
+            _mechanism.Activated += delegate
+            {
+                Open();
+            };
+            _mechanism.Deactivated += delegate
+            {
+                Close();
+            };
+        }
+    }
+
+    public void Open()
+    {
+        _animator.SetTrigger("Open");
+    }
+    public void Close()
+    {
+        _animator.SetTrigger("Close");
+    }
+
+}
---
+++ Assets/Scripts/Props/Mechanisms/OrbSwitch.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class OrbSwitch : Mechanism
+{
+    [SerializeField]
+    private Renderer[] _glassRenderers;
+    [SerializeField]
+    private float _emissionLevel = 0f;
+
+    [SerializeField]
+    private bool _togglable = false;
+
+    private Material[] _glassMaterials;
+    private Color[] _ogEmissionColors;
+    private Damagable _damagable;
+    private Animator _animator;
+
+    private float _previousEmissionLevel = 0f;
+
+    private void Awake()
+    {
+        _damagable = GetComponent<Damagable>();
+        _damagable.TookDamage += _damagable_TookDamage;
+        _animator = GetComponent<Animator>();
+
+        _glassMaterials = new Material[_glassRenderers.Length];
+        _ogEmissionColors = new Color[_glassRenderers.Length];
+        for (int i = 0; i < _glassRenderers.Length; i++)
+        {
+            _glassMaterials[i] = _glassRenderers[i].material;
+            _ogEmissionColors[i] = _glassMaterials[i].GetColor("_EmissionColor");
+        }
+        _previousEmissionLevel = _emissionLevel;
+        SetGlassEmission(_emissionLevel);
+    }
+
+    private void _damagable_TookDamage(float baseDamage, GameObject damageCauser, DamageType damageType)
+    {
+        _animator.SetTrigger("Activate");
+        OnActivate();
+    }
+
+    private void Update()
+    {
+        if (_emissionLevel != _previousEmissionLevel)
+        {
+            SetGlassEmission(_emissionLevel);
+            _previousEmissionLevel = _emissionLevel;
+        }
+    }
+
+    private void SetGlassEmission(float value)
+    {
+        for (int i = 0; i < _glassMaterials.Length; i++)
+        {
+            var color = Color.Lerp(Color.black, _ogEmissionColors[i], value);
+            _glassMaterials[i].SetColor("_EmissionColor", color);
+        }
+    }
+}
