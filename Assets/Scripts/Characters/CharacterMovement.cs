c8bac8a987797dcf77c8a2490dcfa8efbbf980b2
Matthew Thompson <chatrat12@gmail.com>
2017-11-12 00:52:01 -0500

    Removed unwanted debug log

--- Assets/Scripts/Characters/CharacterMovement.cs
+++ Assets/Scripts/Characters/CharacterMovement.cs
@@ -77,8 +77,6 @@ public class CharacterMovement
 
     public virtual void FixedUpdate()
     {
-        Debug.Log(_rigidbody.velocity);
-
         if (!_beingKnockedback && CanMove && _lookTowardsVelocity)
         {
             _character.transform.rotation = _targetRotation;
